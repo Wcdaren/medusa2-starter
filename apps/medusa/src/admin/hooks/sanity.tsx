be746077f019c70d145d4dccf34d71867a4015ca
wcdaren <wcdaren@gmail.com>
2024-11-25 12:10:44 +0800

    feat: add Sanity integration with product sync functionality and related hooks

---
+++ apps/medusa/src/admin/hooks/sanity.tsx
@@ -0,0 +1,63 @@
+import {
+  QueryKey,
+  useMutation,
+  UseMutationOptions,
+  useQuery,
+  useQueryClient,
+  UseQueryOptions,
+} from '@tanstack/react-query'
+import { sdk } from '../lib/sdk'
+import { FetchError } from '@medusajs/js-sdk'
+
+export const useTriggerSanityProductSync = (
+  id: string,
+  options?: UseMutationOptions,
+) => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: () =>
+      sdk.client.fetch(`/admin/sanity/documents/${id}/sync`, {
+        method: 'post',
+      }),
+    onSuccess: (data: any, variables: any, context: any) => {
+      queryClient.invalidateQueries({
+        queryKey: [`sanity_document`, `sanity_document_${id}`],
+      })
+
+      options?.onSuccess?.(data, variables, context)
+    },
+    ...options,
+  })
+}
+
+export const useSanityDocument = (
+  id: string,
+  query?: Record<any, any>,
+  options?: Omit<
+    UseQueryOptions<
+      Record<any, any>,
+      FetchError,
+      { sanity_document: Record<any, any>; studio_url: string },
+      QueryKey
+    >,
+    'queryKey' | 'queryFn'
+  >,
+) => {
+  const fetchSanityProductStatus = async (query?: Record<any, any>) => {
+    return await sdk.client.fetch<Record<any, any>>(
+      `/admin/sanity/documents/${id}`,
+      {
+        query,
+      },
+    )
+  }
+
+  const { data, ...rest } = useQuery({
+    queryFn: async () => fetchSanityProductStatus(query),
+    queryKey: [`sanity_document_${id}`],
+    ...options,
+  })
+
+  return { ...data, ...rest }
+}
